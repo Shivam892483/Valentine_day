1f5726fb78e60f6f09b1ef7d81a34cb9c5f5475d
Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ <111850575+Shivam892483@users.noreply.github.com>
2024-02-12 23:54:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -90,7 +90,7 @@ const Footer = () => {
   return (
     <a
       className="fixed bottom-2 right-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
-      <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+      src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif"
     >
       click Made with{" "}
       <span role="img" aria-label="heart">
